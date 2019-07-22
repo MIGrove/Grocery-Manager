2cf192c7e826e484c08d57e5c0ae82ea3aba8fa3
Matthew Grove <32196181+MIGrove@users.noreply.github.com>
2019-07-22 17:08:52 +0300

    Progress towards exporting to JTable
    lmao nah

--- src/LocatorWindow.java
+++ src/LocatorWindow.java
@@ -32,18 +32,13 @@ public class LocatorWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable_Stores = new javax.swing.JTable();
-        jLabel1 = new javax.swing.JLabel();
-        jComboBox_Sort = new javax.swing.JComboBox<>();
-        jButton_ReturnToLogInWindow = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
-
-        jTable_Stores.setModel(new javax.swing.table.DefaultTableModel(
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -54,95 +49,37 @@ public class LocatorWindow extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable_Stores);
+        jScrollPane2.setViewportView(jTable1);
 
-        jLabel1.setText("Sort stores by:");
-
-        jComboBox_Sort.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Distance", "Lowest price", "Highest price", "Least number of stores necessary" }));
-        jComboBox_Sort.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jComboBox_SortActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
-
-        jButton_ReturnToLogInWindow.setText("Log in again");
-        jButton_ReturnToLogInWindow.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jButton_ReturnToLogInWindow.setContentAreaFilled(false);
-        jButton_ReturnToLogInWindow.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton_ReturnToLogInWindowActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
-                            .addComponent(jComboBox_Sort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jButton_ReturnToLogInWindow, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 216, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jComboBox_Sort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
-                        .addComponent(jButton_ReturnToLogInWindow))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                .addContainerGap())
-        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(0, 0, 0)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 208, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jButton1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton_ReturnToLogInWindowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_ReturnToLogInWindowActionPerformed
-        LoginWindow.discardWindow(this);
-        LoginWindow logWin = new LoginWindow();
-        LoginWindow.showWindow(logWin);
-    }//GEN-LAST:event_jButton_ReturnToLogInWindowActionPerformed
-
-    private void jComboBox_SortActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox_SortActionPerformed
-        System.out.println(jComboBox_Sort.getSelectedItem().toString());
-        
-        //Change jTable_Stores depending on option selected
-        DatabaseManager dbMan = new DatabaseManager();
-        
-        switch (jComboBox_Sort.getSelectedItem().toString()) {
-            case "Lowest price":
-                //use model created here
-        }
-    }//GEN-LAST:event_jComboBox_SortActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        TableModelMaker tModelMaker = new TableModelMaker(jTable1, "SELECT * FROM tblItems;", "GroceryManager.accdb");
+        tModelMaker.getDefaultModel();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -180,12 +117,9 @@ public class LocatorWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton_ReturnToLogInWindow;
-    private javax.swing.JComboBox<String> jComboBox_Sort;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable_Stores;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
 
--- src/TableModelMaker.java
+++ src/TableModelMaker.java
@@ -1,22 +1,38 @@
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 public class TableModelMaker {
+    private DefaultTableModel model;
     
-    public TableModelMaker() {
+    public TableModelMaker(JTable leaderboard, String query, String database) {
+        model = (DefaultTableModel) leaderboard.getModel();
+        model.setRowCount(0);
         
+        for (int i=0; i<convertTableTo2DArray(query, database).length; i++) {
+            model.addRow((convertTableTo2DArray(query, database))[i]);
+        }
+        
+        model.fireTableDataChanged();
     }
     
-    public String[][] convertTableTo2DArray(String query, String database) {
+    private static String[][] convertTableTo2DArray(String query, String database) {
         DatabaseManager dbMan = new DatabaseManager(database);
         int numColumns = dbMan.getNumColumns(query);
         int numRows = dbMan.getNumRows(query);
         
-        String[][] data = new String[numColumns][numRows];
+        String[][] data = new String[numRows][numColumns];
         
-        for (int i = 1; i <= numRows; i++) {
-            for (int j = 1; j <= numColumns; j++) {
+        for (int i = 1; i < numRows; i++) {
+            for (int j = 1; j < numColumns; j++) {
                 data[i][j] = dbMan.getString(query, j, i);
             }
         }
         
         return data;
+    }
+    
+    public DefaultTableModel getDefaultModel() {
+        return model;
     }
 }
